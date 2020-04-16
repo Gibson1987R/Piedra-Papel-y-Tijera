99a347646b06085b4c972cdd2939a3613f68d3a6
Gibson Rosales <gibsonrosales@gmail.com>
2020-04-15 19:50:27 -0430

    Inicio con PPT 1er commit al master

---
+++ src/Juego__v__2.js
@@ -0,0 +1,91 @@
+const victoria = 'Ganaste!!!! ;)';
+
+const derrota = 'Perdiste! :(';
+
+const elementos = ['Piedra', 'Papel', 'Tijera'];
+
+const igualando = (maquina, user) => {
+    var cajitaFeliz = document.createElement('div');
+    //creando el texto del nodo
+    var texto = document.createTextNode(`Empataron...La máquina y Tu elegido: ${maquina} = ${user}`)
+        //integrandolo a la etiqueta
+    cajitaFeliz.appendChild(texto);
+    //añadiendo un atributo
+    cajitaFeliz.setAttribute('class', 'mih1');
+    cajitaFeliz.setAttribute('id', 'emerge');
+    //colocandolo en su contenedor
+    var cont = document.getElementById('midiv-1');
+    cont.appendChild(cajitaFeliz);
+}
+
+const NotaGanadora = (user, maquina, victoria) => {
+    var cajitaFeliz = document.createElement('div');
+    var texto = document.createTextNode(`Felicidades ${victoria} Escogiste ${user}
+    y la máquina perdio al elegir ${maquina}`)
+    cajitaFeliz.appendChild(texto);
+    cajitaFeliz.setAttribute('class', 'necio');
+    cajitaFeliz.setAttribute('id', 'emerge');
+    var cont = document.getElementById('midiv-1');
+    cont.appendChild(cajitaFeliz);
+}
+
+const NotaPerdedora = (user, maquina, derrota) => {
+    var cajitaFeliz = document.createElement('div');
+    var texto = document.createTextNode(`Lo siento ${derrota} Escogiste ${user}
+    y la máquina ganó al elegir ${maquina} Vuelve a intentarlo`)
+    cajitaFeliz.appendChild(texto);
+    cajitaFeliz.setAttribute('class', 'necio');
+    cajitaFeliz.setAttribute('id', 'emerge');
+    var cont = document.getElementById('midiv-1');
+    cont.appendChild(cajitaFeliz);
+}
+
+function inicio() {
+    const Piedrota = document.getElementById('piedra')
+    const Tijerota = document.getElementById('tijera')
+    const Papelote = document.getElementById('papel')
+    Piedrota.addEventListener('click', eleccion);
+    Papelote.addEventListener('click', eleccion);
+    Tijerota.addEventListener('click', eleccion);
+}
+
+const Ocultar = () => {
+    const btnIntentar = document.getElementById("btnIntentar")
+    btnIntentar.addEventListener('onclick', Ocultar)
+    btnIntentar.classList.add('hide')
+}
+
+const BorrarElemento = () => {
+    const elemento = document.getElementById("emerge")
+    const btnIntentar = document.getElementById("btnIntentar")
+    const pad = document.getElementById("midiv-1")
+    var hijo = pad.lastChild;
+    pad.removeChild(hijo)
+}
+
+function eleccion(user) {
+    maquina = elementos[Math.floor(Math.random() * elementos.length)];
+    switch (true && true) {
+        case maquina === 'Piedra' && user === 'Papel':
+            NotaGanadora(user, maquina, victoria)
+            break;
+        case maquina === 'Tijera' && user === 'Piedra':
+            NotaGanadora(user, maquina, victoria)
+            break;
+        case maquina === 'Papel' && user === 'Tijera':
+            NotaGanadora(user, maquina, victoria)
+            break;
+        case maquina === 'Papel' && user === 'Piedra':
+            NotaPerdedora(user, maquina, derrota);
+            break;
+        case maquina === 'Tijera' && user === 'Papel':
+            NotaPerdedora(user, maquina, derrota);
+            break;
+        case maquina === 'Piedra' && user === 'Tijera':
+            NotaPerdedora(user, maquina, derrota);
+            break;
+        default:
+            igualando(maquina, user)
+            break;
+    }
+}
