6bf98a2cd552bed873db9aa7dbb60a422797a5d4
Sebastian Hurtado <sebassdc@gmail.com>
2020-04-19 18:34:26 -0500

    Adding translations

--- src/Juego__v__2.js
+++ src/Juego__v__2.js
@@ -27,21 +27,30 @@ const decisionTree = {
   },
 };
 
+const spanishChoiceTranslation = {
+  [ROCK]: 'piedra',
+  [PAPER]: 'papel',
+  [SCISSORS]: 'tijeras',
+};
+
 const choose = (userChoice) => {
   const machineChoice = ELEMENTS[Math.floor(Math.random() * ELEMENTS.length)];
 
   const result = decisionTree[userChoice][machineChoice];
 
+  const userChoiceText = spanishChoiceTranslation[userChoice];
+  const machineChoiceText = spanishChoiceTranslation[machineChoice];
+
   const textTree = {
-    [VICTORY]: `Felicidades!!! Escogiste ${userChoice} y la máquina perdio al elegir ${machineChoice}. Adelante juega otra vez`,
-    [LOSS]: `Lo siento! Escogiste ${userChoice} y la máquina ganó al elegir ${machineChoice} Vuelve a intentarlo`,
-    [TIE]: `A ver, esta vez "Empataron". Tu y la Máquina eligieron igual: ${machineChoice} y ${userChoice}; ¡Vamos Gánale! intentalo de nuevo`,
+    [VICTORY]: `Felicidades!!! Escogiste ${userChoiceText} y la máquina perdio al elegir ${machineChoiceText}. Adelante juega otra vez`,
+    [LOSS]: `Lo siento! Escogiste ${userChoiceText} y la máquina ganó al elegir ${machineChoiceText} Vuelve a intentarlo`,
+    [TIE]: `A ver, esta vez "Empataron". Tu y la Máquina eligieron igual: ${machineChoiceText} y ${userChoiceText}; ¡Vamos Gánale! intentalo de nuevo`,
   };
   const resultMessage = textTree[result];
   console.log(resultMessage);
 };
 
-document.addEventListener('load', () => {
+window.addEventListener('load', () => {
   document.getElementById('rock').addEventListener('click', () => {
     choose(ROCK);
   });
